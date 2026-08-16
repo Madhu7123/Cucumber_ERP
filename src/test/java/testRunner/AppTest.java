aebf7e2d35d1c1dcb5b9a0576428c4d440b2ab6c
Madhu7123 <madhuvss72@gmail.com>
2026-08-16 21:23:36 +0530

    new_feature

--- src/test/java/testRunner/AppTest.java
+++ src/test/java/testRunner/AppTest.java
@@ -1,14 +1,15 @@
 package testRunner;
-
 import org.junit.runner.RunWith;
-
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(dryRun = false,
-features = {"FeatureFiles"},glue = {"stepDefination"},tags="@customers" ,plugin = {"pretty","html:target/report/cucumber.html",
+features = {"FeatureFiles"},
+glue = {"stepDefination"},
+tags="@suppliers, @customers" ,
+//tags="@customers" ,
+plugin = {"pretty","html:target/report/cucumber.html",
 		"junit:target/report/cucumber.xml",
 		"json:target/report/cucumber.json"})
 
